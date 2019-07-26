ad6a65154a849bc12a06b423183b6736062c4372
Sanjeev Premi <spremi@ymail.com>
2019-07-26 23:12:46 +0530

    feat: s: routes: Add class Fake
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

---
+++ server-py/routes/fake.py
@@ -0,0 +1,19 @@
+#
+# [insight]
+#
+# Sanjeev Premi <spremi@ymail.com>
+#
+# Available under terms of the BSD-3-Clause license.
+#
+
+import logging
+import falcon
+
+log = logging.getLogger('app')
+
+
+class Fake():
+    """
+    Provides 'fake' data enpoints.
+    """
+    pass
