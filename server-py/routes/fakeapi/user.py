36d3cddb486fe7c0e42448d2483815c4246c27e4
Sanjeev Premi <spremi@ymail.com>
2019-07-26 23:29:38 +0530

    feat: s: routes: fake: Add top level handlers
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

---
+++ server-py/routes/fakeapi/user.py
@@ -0,0 +1,25 @@
+#
+# [insight]
+#
+# Sanjeev Premi <spremi@ymail.com>
+#
+# Available under terms of the BSD-3-Clause license.
+#
+
+import logging
+import falcon
+
+log = logging.getLogger('app')
+
+
+class User():
+    """
+    Handle requests related to user(s).
+    """
+
+    def on_get(self, req: falcon.Request, res: falcon.Response, userId: str = None, resource: str = None):
+        log.debug('[FAKE] Users')
+
+        res.body = 'USERS'
+        res.status = falcon.HTTP_200
+        res.content_type = 'text/plain; charset=utf-8'
