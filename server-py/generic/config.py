151f706d5e096ede88c4a1c599e577647be36fe4
Sanjeev Premi <spremi@ymail.com>
2019-07-26 23:06:43 +0530

    feat: s: Add server configuration
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

---
+++ server-py/generic/config.py
@@ -0,0 +1,22 @@
+#
+# [insight]
+#
+# Sanjeev Premi <spremi@ymail.com>
+#
+# Available under terms of the BSD-3-Clause license.
+#
+
+"""
+Server mode. One of 'FAKE' or 'JIRA'.
+"""
+MODE = 'FAKE'
+
+"""
+URL of JIRA server.
+"""
+SERVER_URL = 'https://server.path/'
+
+"""
+Authorization scheme to be used. One of 'COOKIE' or 'BEARER'
+"""
+SERVER_AUTH_SCHEME = 'COOKIE'
