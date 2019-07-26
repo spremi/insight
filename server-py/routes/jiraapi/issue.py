55f547a00e2f8432882bd4c8f66c6aaa831b42e9
Sanjeev Premi <spremi@ymail.com>
2019-07-26 23:36:21 +0530

    feat: s: routes: jira: Add top level handlers
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

---
+++ server-py/routes/jiraapi/issue.py
@@ -0,0 +1,25 @@
+#
+# [insight]
+#
+# Sanjeev Premi <spremi@ymail.com>
+#
+# Available under terms of the BSD-3-Clause license.
+#
+
+import logging
+import falcon
+
+log = logging.getLogger('app')
+
+
+class Issue():
+    """
+    Handle requests related to issue(s).
+    """
+
+    def on_get(self, req: falcon.Request, res: falcon.Response, issueId: str = None, resource: str = None):
+        log.debug('[JIRA] Issues')
+
+        res.body = 'ISSUES'
+        res.status = falcon.HTTP_200
+        res.content_type = 'text/plain; charset=utf-8'
