5c62f97ab24d6cf479ca534858b2ade66df86e80
Sanjeev Premi <spremi@ymail.com>
2019-07-30 15:28:35 +0530

    feat: s: routes: fake: Implement login, logout
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- server-py/routes/fakeapi/auth.py
+++ server-py/routes/fakeapi/auth.py
@@ -6,6 +6,7 @@
 # Available under terms of the BSD-3-Clause license.
 #
 
+import json
 import logging
 import falcon
 
@@ -17,12 +18,18 @@ class AuthLogin():
     Handle login requests.
     """
 
-    def on_get(self, req: falcon.Request, res: falcon.Response):
+    def on_post(self, req: falcon.Request, res: falcon.Response):
         log.debug('[FAKE] Login')
 
-        res.body = 'LOGIN'
+        result = {
+            'session': {
+                'name': 'FAKESESSION',
+                'value': 'LOGIN00000',
+            }
+        }
+
+        res.body = json.dumps(result)
         res.status = falcon.HTTP_200
-        res.content_type = 'text/plain; charset=utf-8'
 
 
 class AuthLogout():
@@ -30,7 +37,7 @@ class AuthLogout():
     Handle logout requests.
     """
 
-    def on_get(self, req: falcon.Request, res: falcon.Response):
+    def on_post(self, req: falcon.Request, res: falcon.Response):
         log.debug('[FAKE] Logout')
 
         res.body = 'LOGOUT'
