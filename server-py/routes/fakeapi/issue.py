72dc18089fbcb078f196ff945c37c3195d81fed1
Sanjeev Premi <spremi@ymail.com>
2019-08-01 23:47:44 +0530

    feat: s: routes: fake: Serve issue data
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- server-py/routes/fakeapi/issue.py
+++ server-py/routes/fakeapi/issue.py
@@ -6,12 +6,18 @@
 # Available under terms of the BSD-3-Clause license.
 #
 
-import logging
+import json
 import falcon
+import logging
 
 from generic.Endpoint import Endpoint
 
 log = logging.getLogger('app')
+
+"""
+Path to data files - relative to 'server-py'.
+"""
+DATA_PATH = './routes/fakeapi/data/'
 
 
 class Issue(Endpoint):
@@ -24,6 +30,44 @@ class Issue(Endpoint):
 
         self.showParams(issueId, resource)
 
-        res.body = 'ISSUES'
-        res.status = falcon.HTTP_200
-        res.content_type = 'text/plain; charset=utf-8'
+        if issueId:
+            if resource:
+                self.getIssueResource(res, issueId, resource)
+
+            else:
+                self.getIssueData(res, issueId)
+
+        else:
+            self.getIssueList(res)
+
+    def getIssueList(self, res: falcon.Response):
+        with open(
+                DATA_PATH + 'issue-list.json',
+                'r',
+                encoding='utf8') as f:
+            data = f.read().replace('\n', '')
+
+            res.body = data
+            res.status = falcon.HTTP_200
+            res.content_type = 'application/json; charset=utf-8'
+
+    def getIssueData(self, res: falcon.Response, issueId: str):
+        with open(
+                DATA_PATH + 'issue-list.json',
+                'r',
+                encoding='utf8') as f:
+            data = json.load(f)
+
+            proj = next((obj for obj in data if obj['id'] == issueId), None)
+
+            if proj:
+                res.body = json.dumps(proj)
+                res.status = falcon.HTTP_200
+                res.content_type = 'application/json; charset=utf-8'
+            else:
+                res.body = 'No issue matches issueId ' + issueId + '.'
+                res.status = falcon.HTTP_404
+                res.content_type = 'text/plain; charset=utf-8'
+
+    def getIssueResource(self, res: falcon.Response, projId: str, resource: str):
+        pass
