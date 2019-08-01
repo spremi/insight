dfe6eebe2fb14d0f0236b544c1530443b65df9c4
Sanjeev Premi <spremi@ymail.com>
2019-08-01 23:51:57 +0530

    feat: s: routes: fake: Serve user data
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- server-py/routes/fakeapi/user.py
+++ server-py/routes/fakeapi/user.py
@@ -6,12 +6,18 @@
 # Available under terms of the BSD-3-Clause license.
 #
 
-import logging
+import json
 import falcon
+import logging
 
 from generic.Endpoint import Endpoint
 
 log = logging.getLogger('app')
+
+"""
+Path to data files - relative to 'server-py'.
+"""
+DATA_PATH = './routes/fakeapi/data/'
 
 
 class User(Endpoint):
@@ -24,6 +30,44 @@ class User(Endpoint):
 
         self.showParams(userId, resource)
 
-        res.body = 'USERS'
-        res.status = falcon.HTTP_200
-        res.content_type = 'text/plain; charset=utf-8'
+        if userId:
+            if resource:
+                self.getUserResource(res, userId, resource)
+
+            else:
+                self.getUserData(res, userId)
+
+        else:
+            self.getUserList(res)
+
+    def getUserList(self, res: falcon.Response):
+        with open(
+                DATA_PATH + 'user-list.json',
+                'r',
+                encoding='utf8') as f:
+            data = f.read().replace('\n', '')
+
+            res.body = data
+            res.status = falcon.HTTP_200
+            res.content_type = 'application/json; charset=utf-8'
+
+    def getUserData(self, res: falcon.Response, userId: str):
+        with open(
+                DATA_PATH + 'user-list.json',
+                'r',
+                encoding='utf8') as f:
+            data = json.load(f)
+
+            proj = next((obj for obj in data if obj['id'] == userId), None)
+
+            if proj:
+                res.body = json.dumps(proj)
+                res.status = falcon.HTTP_200
+                res.content_type = 'application/json; charset=utf-8'
+            else:
+                res.body = 'No project matches userId ' + userId + '.'
+                res.status = falcon.HTTP_404
+                res.content_type = 'text/plain; charset=utf-8'
+
+    def getUserResource(self, res: falcon.Response, userId: str, resource: str):
+        pass
