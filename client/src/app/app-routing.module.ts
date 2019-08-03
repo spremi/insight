0bc533585cfb79f182bb5273cb4c009a42c3dbda
Sanjeev Premi <spremi@ymail.com>
2019-08-03 15:24:12 +0530

    feat: c: app: Initial route setup
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,8 +1,57 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { AboutComponent } from './routes/about/about.component';
+import { HomeComponent } from './routes/home/home.component';
+import { IssueComponent } from './routes/issue/issue.component';
+import { LoginComponent } from './routes/login/login.component';
+import { LogoutComponent } from './routes/logout/logout.component';
+import { ProjectComponent } from './routes/project/project.component';
+import { StrayComponent } from './routes/stray/stray.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'logout',
+    component: LogoutComponent,
+  },
+  {
+    path: 'about',
+    component: AboutComponent,
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+  {
+    path: 'issue',
+    component: IssueComponent,
+  },
+  {
+    path: 'issue/:id',
+    component: IssueComponent,
+  },
+  {
+    path: 'project',
+    component: ProjectComponent,
+  },
+  {
+    path: 'project/:id',
+    component: ProjectComponent,
+  },
+  {
+    path: '**',
+    component: StrayComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
