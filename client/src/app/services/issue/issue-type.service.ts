33630bf9c601d4e3152c400b4e448329b2c272a3
Sanjeev Premi <spremi@ymail.com>
2019-08-04 02:04:15 +0530

    chore: c: services: Add 'issue-type'
    Command:
      ng g s services/issue/issue-type --lintFix=true
    
    Also added file headers.
    
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

---
+++ client/src/app/services/issue/issue-type.service.ts
@@ -0,0 +1,17 @@
+//
+// [insight]
+//
+// Sanjeev Premi <spremi@ymail.com>
+//
+// Available under terms of the BSD-3-Clause license.
+//
+
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class IssueTypeService {
+
+  constructor() { }
+}
