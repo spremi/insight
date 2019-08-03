342cec7f8f53d5937c23e6e08d3ea3cd877b473d
Sanjeev Premi <spremi@ymail.com>
2019-08-03 14:54:55 +0530

    chore: c: parts: Add 'issue-list'
    Command:
      ng g c parts/issue-list --style=sass --lintFix=true
    
    Also added file headers.
    
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

---
+++ client/src/app/parts/issue-list/issue-list.component.ts
@@ -0,0 +1,23 @@
+//
+// [insight]
+//
+// Sanjeev Premi <spremi@ymail.com>
+//
+// Available under terms of the BSD-3-Clause license.
+//
+
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'sp-issue-list',
+  templateUrl: './issue-list.component.html',
+  styleUrls: ['./issue-list.component.sass'],
+})
+export class IssueListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
