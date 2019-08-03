2359f1985698fbcb1f75f2c9872010b8d053ea5b
Sanjeev Premi <spremi@ymail.com>
2019-08-03 14:46:34 +0530

    chore: c: routes: Add 'issue'
    Command:
      ng g c routes/issue--style=sass --lintFix=true
    
    Also added file headers.
    
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

---
+++ client/src/app/routes/issue/issue.component.ts
@@ -0,0 +1,23 @@
+//
+// [insight]
+//
+// Sanjeev Premi <spremi@ymail.com>
+//
+// Available under terms of the BSD-3-Clause license.
+//
+
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'sp-issue',
+  templateUrl: './issue.component.html',
+  styleUrls: ['./issue.component.sass'],
+})
+export class IssueComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
