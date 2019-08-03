3a2fb4373b982e54058df2db310071cff203f79d
Sanjeev Premi <spremi@ymail.com>
2019-08-03 15:12:49 +0530

    chore: c: modules: Add module 'material'
    Command:
      ng g m modules/material --lintFix=true
    
    Also added file headers.
    
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

---
+++ client/src/app/modules/material/material.module.ts
@@ -0,0 +1,19 @@
+//
+// [insight]
+//
+// Sanjeev Premi <spremi@ymail.com>
+//
+// Available under terms of the BSD-3-Clause license.
+//
+
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+  ],
+})
+export class MaterialModule { }
