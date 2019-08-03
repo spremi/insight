88fbb95451e2b311f486e96c2e565b1de6ef3ef4
Sanjeev Premi <spremi@ymail.com>
2019-08-03 14:58:45 +0530

    chore: c: parts: Add 'header'
    Command:
      ng g c parts/header --style=sass --lintFix=true
    
    Also added file headers.
    
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

---
+++ client/src/app/parts/header/header.component.ts
@@ -0,0 +1,23 @@
+//
+// [insight]
+//
+// Sanjeev Premi <spremi@ymail.com>
+//
+// Available under terms of the BSD-3-Clause license.
+//
+
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'sp-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.sass'],
+})
+export class HeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
