df05b2da02082302e1506517e559ac9ee481c93f
Sanjeev Premi <spremi@ymail.com>
2019-08-03 15:28:09 +0530

    feat: c: parts: footer: Initial implementation
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- client/src/app/parts/footer/footer.component.spec.ts
+++ client/src/app/parts/footer/footer.component.spec.ts
@@ -8,6 +8,8 @@
 
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from 'src/app/modules/material/material.module';
 import { FooterComponent } from './footer.component';
 
 describe('FooterComponent', () => {
@@ -16,6 +18,10 @@ describe('FooterComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [
+        NoopAnimationsModule,
+        MaterialModule,
+      ],
       declarations: [FooterComponent],
     })
       .compileComponents();
