2aa451fdd34013609c420b28a5d8e5cd8817694b
Sanjeev Premi <spremi@ymail.com>
2019-08-03 14:56:21 +0530

    chore: c: parts: Add 'issue-info'
    Command:
      ng g c parts/issue-info --style=sass --lintFix=true
    
    Also added file headers.
    
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

---
+++ client/src/app/parts/issue-info/issue-info.component.ts
@@ -0,0 +1,23 @@
+//
+// [insight]
+//
+// Sanjeev Premi <spremi@ymail.com>
+//
+// Available under terms of the BSD-3-Clause license.
+//
+
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'sp-issue-info',
+  templateUrl: './issue-info.component.html',
+  styleUrls: ['./issue-info.component.sass'],
+})
+export class IssueInfoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
