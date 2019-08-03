108f987dd623b05fabe45051a63119b5d9e9059d
Sanjeev Premi <spremi@ymail.com>
2019-08-03 17:31:40 +0530

    feat: c: parts: project-list: Initial implementation
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- client/src/app/parts/project-list/project-list.component.ts
+++ client/src/app/parts/project-list/project-list.component.ts
@@ -6,7 +6,8 @@
 // Available under terms of the BSD-3-Clause license.
 //
 
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Project } from 'src/app/models/project';
 
 @Component({
   selector: 'sp-project-list',
@@ -14,10 +15,56 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./project-list.component.sass'],
 })
 export class ProjectListComponent implements OnInit {
+  @Input()
+  projects: Project[];
+
+  /**
+   * Dictionary mapping project-id to checkbox state.
+   */
+  options: Map<string, boolean> = new Map();
+
+  /**
+   * Filter text for project name(s).
+   */
+  filterText: string;
+
+  /**
+   * Number of projects selected.
+   */
+  selected = 0;
 
   constructor() { }
 
   ngOnInit() {
+    if (this.projects) {
+      this.projects.map((project) => {
+        this.options.set(project.id, false);
+      });
+    }
   }
 
+  toggleProject(id: string) {
+    const flag = !this.options.get(id);
+
+    this.options.set(id, flag);
+
+    if (flag) {
+      this.selected++;
+    } else {
+      this.selected--;
+    }
+  }
+
+  setFavorites() {
+    const selection: string[] = [];
+
+    this.options.forEach((v, k) => {
+      if (v) {
+        selection.push(k);
+      }
+    });
+
+    console.log('Dispatch action');
+    console.log(selection);
+  }
 }
