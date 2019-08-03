7450305c76352aa0e8fd026da05bf54eeddad572
Sanjeev Premi <spremi@ymail.com>
2019-08-03 17:17:03 +0530

    chore: c: parts: Add 'project-card'
    Command:
      ng g c parts/project-card --style=sass --lintFix=true
    
    Also added file headers.
    
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

---
+++ client/src/app/parts/project-card/project-card.component.ts
@@ -0,0 +1,23 @@
+//
+// [insight]
+//
+// Sanjeev Premi <spremi@ymail.com>
+//
+// Available under terms of the BSD-3-Clause license.
+//
+
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'sp-project-card',
+  templateUrl: './project-card.component.html',
+  styleUrls: ['./project-card.component.sass'],
+})
+export class ProjectCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
