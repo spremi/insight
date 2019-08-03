f8b16306996fc234c7b26ecdc1b7aaa24139fff4
Sanjeev Premi <spremi@ymail.com>
2019-08-03 14:49:47 +0530

    chore: c: parts: Add 'project-list'
    Command:
      ng g c parts/project-list --style=sass --lintFix=true
    
    Also added file headers.
    
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

---
+++ client/src/app/parts/project-list/project-list.component.ts
@@ -0,0 +1,23 @@
+//
+// [insight]
+//
+// Sanjeev Premi <spremi@ymail.com>
+//
+// Available under terms of the BSD-3-Clause license.
+//
+
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'sp-project-list',
+  templateUrl: './project-list.component.html',
+  styleUrls: ['./project-list.component.sass'],
+})
+export class ProjectListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
