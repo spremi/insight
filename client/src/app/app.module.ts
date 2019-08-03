22ec26259602311d07f654585f407a2b20f210e3
Sanjeev Premi <spremi@ymail.com>
2019-08-03 14:44:58 +0530

    chore: c: routes: Add 'about'
    Command:
      ng g c routes/about --style=sass --lintFix=true
    
    Also added file headers.
    
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -6,12 +6,14 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './routes/about/about.component';
 import { HomeComponent } from './routes/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
+    AboutComponent,
   ],
   imports: [
     BrowserModule,
