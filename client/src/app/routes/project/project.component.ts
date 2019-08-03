a41b63b84d934015228cc31f43e41e71a56ff7e7
Sanjeev Premi <spremi@ymail.com>
2019-08-03 14:45:42 +0530

    chore: c: routes: Add 'project'
    Command:
      ng g c routes/project--style=sass --lintFix=true
    
    Also added file headers.
    
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

---
+++ client/src/app/routes/project/project.component.ts
@@ -0,0 +1,23 @@
+//
+// [insight]
+//
+// Sanjeev Premi <spremi@ymail.com>
+//
+// Available under terms of the BSD-3-Clause license.
+//
+
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'sp-project',
+  templateUrl: './project.component.html',
+  styleUrls: ['./project.component.sass'],
+})
+export class ProjectComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
