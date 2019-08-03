f731d175f77699bdcfc529e1ecd32c4b342af851
Sanjeev Premi <spremi@ymail.com>
2019-08-03 14:52:28 +0530

    chore: c: parts: Add 'project-info'
    Command:
      ng g c parts/project-info --style=sass --lintFix=true
    
    Also added file headers.
    
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

---
+++ client/src/app/parts/project-info/project-info.component.spec.ts
@@ -0,0 +1,33 @@
+//
+// [insight]
+//
+// Sanjeev Premi <spremi@ymail.com>
+//
+// Available under terms of the BSD-3-Clause license.
+//
+
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ProjectInfoComponent } from './project-info.component';
+
+describe('ProjectInfoComponent', () => {
+  let component: ProjectInfoComponent;
+  let fixture: ComponentFixture<ProjectInfoComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ProjectInfoComponent],
+    })
+      .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ProjectInfoComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ client/src/app/parts/project-info/project-info.component.ts
@@ -0,0 +1,23 @@
+//
+// [insight]
+//
+// Sanjeev Premi <spremi@ymail.com>
+//
+// Available under terms of the BSD-3-Clause license.
+//
+
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'sp-project-info',
+  templateUrl: './project-info.component.html',
+  styleUrls: ['./project-info.component.sass'],
+})
+export class ProjectInfoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
