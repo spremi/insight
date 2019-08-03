9fd6ff259ce7697f95aa75868884860649425572
Sanjeev Premi <spremi@ymail.com>
2019-08-03 14:59:56 +0530

    chore: c: parts: Add 'footer'
    Command:
      ng g c parts/footer --style=sass --lintFix=true
    
    Also added file headers.
    
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

---
+++ client/src/app/parts/footer/footer.component.ts
@@ -0,0 +1,23 @@
+//
+// [insight]
+//
+// Sanjeev Premi <spremi@ymail.com>
+//
+// Available under terms of the BSD-3-Clause license.
+//
+
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'sp-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.sass'],
+})
+export class FooterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
