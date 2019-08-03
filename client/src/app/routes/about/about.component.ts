e6d0c879ec09349f853879b89b8383bf319e436b
Sanjeev Premi <spremi@ymail.com>
2019-08-03 14:44:58 +0530

    chore: c: routes: Add 'about'
    Command:
      ng g c routes/about --style=sass --lintFix=true
    
    Also added file headers.
    
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

---
+++ client/src/app/routes/about/about.component.ts
@@ -0,0 +1,23 @@
+//
+// [insight]
+//
+// Sanjeev Premi <spremi@ymail.com>
+//
+// Available under terms of the BSD-3-Clause license.
+//
+
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'sp-about',
+  templateUrl: './about.component.html',
+  styleUrls: ['./about.component.sass'],
+})
+export class AboutComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
