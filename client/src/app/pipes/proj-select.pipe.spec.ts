3404676f42ddd4b16c47f39eb7f08d317c9c92c9
Sanjeev Premi <spremi@ymail.com>
2019-08-03 17:21:34 +0530

    chore: c: pipes: Add 'proj-select'
    Command:
      ng g pipe pipes/proj-select --lintFix=true
    
    Also added file headers.
    
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

---
+++ client/src/app/pipes/proj-select.pipe.spec.ts
@@ -0,0 +1,16 @@
+//
+// [insight]
+//
+// Sanjeev Premi <spremi@ymail.com>
+//
+// Available under terms of the BSD-3-Clause license.
+//
+
+import { ProjSelectPipe } from './proj-select.pipe';
+
+describe('ProjSelectPipe', () => {
+  it('create an instance', () => {
+    const pipe = new ProjSelectPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
