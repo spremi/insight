dd6fc0f2b4ee081f7849861b1718481062c08a4c
Sanjeev Premi <spremi@ymail.com>
2019-08-06 00:59:32 +0530

    feat: c: svc: issue-priority: Initial implementation
    Just necessary to get started on other components.
    
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- client/src/app/services/issue/issue-priority.service.ts
+++ client/src/app/services/issue/issue-priority.service.ts
@@ -7,11 +7,52 @@
 //
 
 import { Injectable } from '@angular/core';
+import { of, EMPTY, Observable } from 'rxjs';
+import { IssueElement } from 'src/app/models/issue';
 
 @Injectable({
   providedIn: 'root',
 })
 export class IssuePriorityService {
+  /**
+   * Dummy issue priority.
+   */
+  private list: IssueElement[] = [
+    {
+      id: 'prio_01',
+      name: 'PRIO 01',
+      desc: 'Priority 001',
+      iconId: 'prio_01_icon',
+    },
+    {
+      id: 'prio_02',
+      name: 'PRIO 02',
+      desc: 'Priority 002',
+      iconId: 'prio_02_icon',
+    },
+    {
+      id: 'prio_03',
+      name: 'PRIO 03',
+      desc: 'Priority 003',
+      iconId: 'prio_03_icon',
+    },
+  ];
 
   constructor() { }
+
+  /**
+   * Returns observable to the 'list'.
+   */
+  public getAll(): Observable<IssueElement[]> {
+    return of(this.list);
+  }
+
+  /**
+   * Returns observable to object matching specified 'id'.
+   */
+  public getOne(id: string): Observable<IssueElement> {
+    const issue = this.list.find(item => item.id === id);
+
+    return issue ? of(issue) : EMPTY;
+  }
 }
