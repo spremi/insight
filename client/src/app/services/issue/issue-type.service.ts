75d179405e573eaec30f7b8581ad85f347973751
Sanjeev Premi <spremi@ymail.com>
2019-08-06 00:32:53 +0530

    feat: c: svc: issue-type: Initial implementation
    Just necessary to get started on other components.
    
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- client/src/app/services/issue/issue-type.service.ts
+++ client/src/app/services/issue/issue-type.service.ts
@@ -7,11 +7,52 @@
 //
 
 import { Injectable } from '@angular/core';
+import { of, EMPTY, Observable } from 'rxjs';
+import { IssueElement } from 'src/app/models/issue';
 
 @Injectable({
   providedIn: 'root',
 })
 export class IssueTypeService {
+  /**
+   * Dummy issue types.
+   */
+  private list: IssueElement[] = [
+    {
+      id: 'type_01',
+      name: 'TYPE 01',
+      desc: 'Issue type 001',
+      iconId: 'type_01_icon',
+    },
+    {
+      id: 'type_02',
+      name: 'TYPE 02',
+      desc: 'Issue type 002',
+      iconId: 'type_02_icon',
+    },
+    {
+      id: 'type_03',
+      name: 'TYPE 03',
+      desc: 'Issue type 003',
+      iconId: 'type_03_icon',
+    },
+  ];
 
   constructor() { }
+
+  /**
+   * Returns observable to the 'list'.
+   */
+  public getAll(): Observable<IssueElement[]> {
+    return of(this.list);
+  }
+
+  /**
+   * Returns observable to object matching specified 'id'.
+   */
+  public getOne(id: string): Observable<IssueElement> {
+    const issue = this.list.find(item => item.id === id);
+
+    return issue ? of(issue) : EMPTY;
+  }
 }
