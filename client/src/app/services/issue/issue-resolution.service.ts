0bcca19c2b3fc180c1545602ce2b9dbf54bd33ca
Sanjeev Premi <spremi@ymail.com>
2019-08-06 00:54:12 +0530

    feat: c: svc: issue-resolution: Initial implementation
    Just necessary to get started on other components.
    
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- client/src/app/services/issue/issue-resolution.service.ts
+++ client/src/app/services/issue/issue-resolution.service.ts
@@ -7,11 +7,52 @@
 //
 
 import { Injectable } from '@angular/core';
+import { of, EMPTY, Observable } from 'rxjs';
+import { IssueElement } from 'src/app/models/issue';
 
 @Injectable({
   providedIn: 'root',
 })
 export class IssueResolutionService {
+  /**
+   * Dummy issue resolutions.
+   */
+  private list: IssueElement[] = [
+    {
+      id: 'reso_01',
+      name: 'RESO 01',
+      desc: 'Resolution 001',
+      iconId: 'reso_01_icon',
+    },
+    {
+      id: 'reso_02',
+      name: 'RESO 02',
+      desc: 'Resolution 002',
+      iconId: 'reso_02_icon',
+    },
+    {
+      id: 'reso_03',
+      name: 'RESO 03',
+      desc: 'Resolution 003',
+      iconId: 'reso_03_icon',
+    },
+  ];
 
   constructor() { }
+
+  /**
+   * Returns observable to the 'list'.
+   */
+  public getAll(): Observable<IssueElement[]> {
+    return of(this.list);
+  }
+
+  /**
+   * Returns observable to object matching specified 'id'.
+   */
+  public getOne(id: string): Observable<IssueElement> {
+    const issue = this.list.find(item => item.id === id);
+
+    return issue ? of(issue) : EMPTY;
+  }
 }
