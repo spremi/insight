48bc7f6688425f3223aa9ac4831dbb4c10dc2d62
Sanjeev Premi <spremi@ymail.com>
2019-08-06 00:37:26 +0530

    feat: c: svc: issue-status: Initial implementation
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- client/src/app/services/issue/issue-status.service.ts
+++ client/src/app/services/issue/issue-status.service.ts
@@ -7,11 +7,52 @@
 //
 
 import { Injectable } from '@angular/core';
+import { of, EMPTY, Observable } from 'rxjs';
+import { IssueElement } from 'src/app/models/issue';
 
 @Injectable({
   providedIn: 'root',
 })
 export class IssueStatusService {
+  /**
+   * Dummy issue statuses.
+   */
+  private list: IssueElement[] = [
+    {
+      id: 'stat_01',
+      name: 'STAT 01',
+      desc: 'Issue status 001',
+      iconId: 'stat_01_icon',
+    },
+    {
+      id: 'stat_02',
+      name: 'STAT 02',
+      desc: 'Issue status 002',
+      iconId: 'stat_02_icon',
+    },
+    {
+      id: 'stat_03',
+      name: 'STAT 03',
+      desc: 'Issue status 003',
+      iconId: 'stat_03_icon',
+    },
+  ];
 
   constructor() { }
+
+  /**
+   * Returns observable to the 'list'.
+   */
+  public getAll(): Observable<IssueElement[]> {
+    return of(this.list);
+  }
+
+  /**
+   * Returns observable to object matching specified 'id'.
+   */
+  public getOne(id: string): Observable<IssueElement> {
+    const issue = this.list.find(item => item.id === id);
+
+    return issue ? of(issue) : EMPTY;
+  }
 }
