92f44e5416b1fa340768557f9e2d3410ca9daed5
Sanjeev Premi <spremi@ymail.com>
2019-08-07 00:07:03 +0530

    chore: c: parts: Add 'project-summary'
    Command:
      ng g c parts/project-summary --style=sass --lintFix=true
    
    Also added file headers.
    
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

---
+++ client/src/app/parts/project-summary/project-summary.component.spec.ts
@@ -0,0 +1,33 @@
+//
+// [insight]
+//
+// Sanjeev Premi <spremi@ymail.com>
+//
+// Available under terms of the BSD-3-Clause license.
+//
+
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ProjectSummaryComponent } from './project-summary.component';
+
+describe('ProjectSummaryComponent', () => {
+  let component: ProjectSummaryComponent;
+  let fixture: ComponentFixture<ProjectSummaryComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ProjectSummaryComponent],
+    })
+      .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ProjectSummaryComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
