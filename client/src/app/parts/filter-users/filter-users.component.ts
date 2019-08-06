8321532692d34880de0e72d3070c6ece8038a5ec
Sanjeev Premi <spremi@ymail.com>
2019-08-07 01:25:43 +0530

    feat: c: parts: filter-users: Initial implementation
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- client/src/app/parts/filter-users/filter-users.component.ts
+++ client/src/app/parts/filter-users/filter-users.component.ts
@@ -7,6 +7,8 @@
 //
 
 import { Component, OnInit } from '@angular/core';
+import { MatListOption } from '@angular/material/list';
+import { User } from 'src/app/models/user';
 
 @Component({
   selector: 'sp-filter-users',
@@ -14,10 +16,48 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./filter-users.component.sass'],
 })
 export class FilterUsersComponent implements OnInit {
+  /**
+   * Dummy list of users in project.
+   */
+  list: User[] = [
+    {
+      id: 'user01',
+      name: 'User 1',
+      avatarId: 'av001',
+      active: true,
+    },
+    {
+      id: 'comp02',
+      name: 'User 2',
+      avatarId: 'av002',
+      active: true,
+    },
+    {
+      id: 'comp03',
+      name: 'User 3',
+      avatarId: 'av003',
+      active: true,
+    },
+    {
+      id: 'comp04',
+      name: 'User 4',
+      avatarId: 'av004',
+      active: true,
+    },
+    {
+      id: 'comp05',
+      name: 'User 5',
+      avatarId: 'av005',
+      active: true,
+    },
+  ];
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  applyFilter(selected: MatListOption[]) {
+    console.log('TODO: Dispatch action');
+  }
 }
