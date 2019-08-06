2408da600e6dcba6a40f3c05b63e0975fde8c1f0
Sanjeev Premi <spremi@ymail.com>
2019-08-07 01:36:31 +0530

    feat: c: parts: filter-priority: Initial implementation
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- client/src/app/parts/filter-priority/filter-priority.component.spec.ts
+++ client/src/app/parts/filter-priority/filter-priority.component.spec.ts
@@ -8,6 +8,8 @@
 
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from 'src/app/modules/material/material.module';
 import { FilterPriorityComponent } from './filter-priority.component';
 
 describe('FilterPriorityComponent', () => {
@@ -16,6 +18,10 @@ describe('FilterPriorityComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [
+        NoopAnimationsModule,
+        MaterialModule,
+      ],
       declarations: [FilterPriorityComponent],
     })
       .compileComponents();
