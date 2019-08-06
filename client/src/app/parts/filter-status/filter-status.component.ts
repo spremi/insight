14dec42b53c78064990d9f81dfe6b0bdf8e63dc2
Sanjeev Premi <spremi@ymail.com>
2019-08-07 00:45:10 +0530

    chore: c: parts: Add 'filter-status'
    Command:
      ng g c parts/filter-status --style=sass --lintFix=true
    
    Also added file headers.
    
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

---
+++ client/src/app/parts/filter-status/filter-status.component.ts
@@ -0,0 +1,23 @@
+//
+// [insight]
+//
+// Sanjeev Premi <spremi@ymail.com>
+//
+// Available under terms of the BSD-3-Clause license.
+//
+
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'sp-filter-status',
+  templateUrl: './filter-status.component.html',
+  styleUrls: ['./filter-status.component.sass'],
+})
+export class FilterStatusComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
