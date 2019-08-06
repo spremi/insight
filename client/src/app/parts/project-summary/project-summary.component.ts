b099e44dea39e1d198475379d00e587e67ff7b04
Sanjeev Premi <spremi@ymail.com>
2019-08-07 00:07:03 +0530

    chore: c: parts: Add 'project-summary'
    Command:
      ng g c parts/project-summary --style=sass --lintFix=true
    
    Also added file headers.
    
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

---
+++ client/src/app/parts/project-summary/project-summary.component.ts
@@ -0,0 +1,23 @@
+//
+// [insight]
+//
+// Sanjeev Premi <spremi@ymail.com>
+//
+// Available under terms of the BSD-3-Clause license.
+//
+
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'sp-project-summary',
+  templateUrl: './project-summary.component.html',
+  styleUrls: ['./project-summary.component.sass'],
+})
+export class ProjectSummaryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
