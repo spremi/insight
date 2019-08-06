aaebfc94a84c3d67f12dd12e580f235682108b7a
Sanjeev Premi <spremi@ymail.com>
2019-08-07 00:47:31 +0530

    chore: c: parts: Add 'filter-resolution'
    Command:
      ng g c parts/filter-resolution --style=sass --lintFix=true
    
    Also added file headers.
    
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

---
+++ client/src/app/parts/filter-resolution/filter-resolution.component.ts
@@ -0,0 +1,23 @@
+//
+// [insight]
+//
+// Sanjeev Premi <spremi@ymail.com>
+//
+// Available under terms of the BSD-3-Clause license.
+//
+
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'sp-filter-resolution',
+  templateUrl: './filter-resolution.component.html',
+  styleUrls: ['./filter-resolution.component.sass'],
+})
+export class FilterResolutionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
