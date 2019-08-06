a118a53f585a376d5f0272256e73faa0e4461477
Sanjeev Premi <spremi@ymail.com>
2019-08-07 01:36:31 +0530

    feat: c: parts: filter-priority: Initial implementation
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- client/src/app/parts/filter-priority/filter-priority.component.ts
+++ client/src/app/parts/filter-priority/filter-priority.component.ts
@@ -7,6 +7,8 @@
 //
 
 import { Component, OnInit } from '@angular/core';
+import { MatListOption } from '@angular/material/list';
+import { IssueElement } from 'src/app/models/issue';
 
 @Component({
   selector: 'sp-filter-priority',
@@ -14,10 +16,48 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./filter-priority.component.sass'],
 })
 export class FilterPriorityComponent implements OnInit {
+  /**
+   * Dummy list of issue priorities.
+   */
+  list: IssueElement[] = [
+    {
+      id: 'prio01',
+      name: 'Prio 1',
+      desc: 'Priority 1',
+      iconId: 'prio_icon_1',
+    },
+    {
+      id: 'prio01',
+      name: 'Prio 1',
+      desc: 'Priority 1',
+      iconId: 'prio_icon_1',
+    },
+    {
+      id: 'prio02',
+      name: 'Prio 2',
+      desc: 'Priority 2',
+      iconId: 'prio_icon_2',
+    },
+    {
+      id: 'prio03',
+      name: 'Prio 3',
+      desc: 'Priority 3',
+      iconId: 'prio_icon_3',
+    },
+    {
+      id: 'prio04',
+      name: 'Prio 4',
+      desc: 'Priority 4',
+      iconId: 'prio_icon_4',
+    },
+  ];
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  applyFilter(selected: MatListOption[]) {
+    console.log('TODO: Dispatch action');
+  }
 }
