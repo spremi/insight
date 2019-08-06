ce4608921a8b5d9ac372509231c50fbfac0a7481
Sanjeev Premi <spremi@ymail.com>
2019-08-07 01:42:16 +0530

    feat: c: parts: filter-resolution: Initial implementation
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- client/src/app/parts/filter-resolution/filter-resolution.component.ts
+++ client/src/app/parts/filter-resolution/filter-resolution.component.ts
@@ -7,6 +7,8 @@
 //
 
 import { Component, OnInit } from '@angular/core';
+import { MatListOption } from '@angular/material/list';
+import { IssueElement } from 'src/app/models/issue';
 
 @Component({
   selector: 'sp-filter-resolution',
@@ -14,10 +16,48 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./filter-resolution.component.sass'],
 })
 export class FilterResolutionComponent implements OnInit {
+  /**
+   * Dummy list of issue resolutions.
+   */
+  list: IssueElement[] = [
+    {
+      id: 'reso01',
+      name: 'Reso 1',
+      desc: 'Resolution 1',
+      iconId: 'reso_icon_1',
+    },
+    {
+      id: 'reso01',
+      name: 'Reso 1',
+      desc: 'Resolution 1',
+      iconId: 'reso_icon_1',
+    },
+    {
+      id: 'reso02',
+      name: 'Reso 2',
+      desc: 'Resolution 2',
+      iconId: 'reso_icon_2',
+    },
+    {
+      id: 'reso03',
+      name: 'Reso 3',
+      desc: 'Resolution 3',
+      iconId: 'reso_icon_3',
+    },
+    {
+      id: 'reso04',
+      name: 'Reso 4',
+      desc: 'Resolution 4',
+      iconId: 'reso_icon_4',
+    },
+  ];
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  applyFilter(selected: MatListOption[]) {
+    console.log('TODO: Dispatch action');
+  }
 }
