f8fb487ff1ec8ec3e0198cc004a6dcc8cbf5565b
Sanjeev Premi <spremi@ymail.com>
2019-08-07 01:42:16 +0530

    feat: c: parts: filter-resolution: Initial implementation
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- client/src/app/parts/filter-resolution/filter-resolution.component.ts
+++ client/src/app/parts/filter-resolution/filter-resolution.component.ts
@@ -7,6 +7,9 @@
 //
 
 import { Component, OnInit } from '@angular/core';
+import { MatListOption } from '@angular/material/list';
+import { IssueElement } from 'src/app/models/issue';
+import { IssueDataService } from 'src/app/services/issue/issue-data.service';
 
 @Component({
   selector: 'sp-filter-resolution',
@@ -14,10 +17,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./filter-resolution.component.sass'],
 })
 export class FilterResolutionComponent implements OnInit {
+  /**
+   * List of issue resoultions.
+   */
+  list: IssueElement[];
 
-  constructor() { }
+  constructor(private issueDataSvc: IssueDataService) { }
 
   ngOnInit() {
+    this.issueDataSvc.getResolutions().subscribe((result) => {
+      this.list = result;
+    });
   }
 
+  applyFilter(selected: MatListOption[]) {
+    console.log('TODO: Dispatch action');
+  }
 }
