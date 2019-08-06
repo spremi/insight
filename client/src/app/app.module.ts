31d0769e16227825a4195c7c6fac79858d656cf7
Sanjeev Premi <spremi@ymail.com>
2019-08-07 00:45:10 +0530

    chore: c: parts: Add 'filter-status'
    Command:
      ng g c parts/filter-status --style=sass --lintFix=true
    
    Also added file headers.
    
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -9,6 +9,7 @@ import { AppComponent } from './app.component';
 import { MaterialModule } from './modules/material/material.module';
 import { FavProjectsComponent } from './parts/fav-projects/fav-projects.component';
 import { FilterComponentsComponent } from './parts/filter-components/filter-components.component';
+import { FilterStatusComponent } from './parts/filter-status/filter-status.component';
 import { FilterUsersComponent } from './parts/filter-users/filter-users.component';
 import { FooterComponent } from './parts/footer/footer.component';
 import { HeaderComponent } from './parts/header/header.component';
@@ -57,6 +58,7 @@ import { StrayComponent } from './routes/stray/stray.component';
     ProjectSummaryComponent,
     FilterUsersComponent,
     FilterComponentsComponent,
+    FilterStatusComponent,
   ],
   imports: [
     BrowserModule,
