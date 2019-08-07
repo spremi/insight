20ed6e0c2c40a4ad410e28035da51ba16e6ecc62
Sanjeev Premi <spremi@ymail.com>
2019-08-07 22:10:31 +0530

    chore: c: parts: Add 'tile'
    Command:
      ng g c parts/tile --style=sass --lintFix=true
    
    Also added file headers.
    
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

---
+++ client/src/app/parts/tile/tile.component.ts
@@ -0,0 +1,23 @@
+//
+// [insight]
+//
+// Sanjeev Premi <spremi@ymail.com>
+//
+// Available under terms of the BSD-3-Clause license.
+//
+
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'sp-tile',
+  templateUrl: './tile.component.html',
+  styleUrls: ['./tile.component.sass'],
+})
+export class TileComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
