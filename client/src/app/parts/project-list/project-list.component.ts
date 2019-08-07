8582e021e8fd4eb8bbbf3105f46254198d4eed91
Sanjeev Premi <spremi@ymail.com>
2019-08-08 03:19:46 +0530

    feat: c: parts: project-list: Use checkboxes
    Better utilization of available space.
    
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- client/src/app/parts/project-list/project-list.component.ts
+++ client/src/app/parts/project-list/project-list.component.ts
@@ -6,7 +6,7 @@
 // Available under terms of the BSD-3-Clause license.
 //
 
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Project } from 'src/app/models/project';
 
 @Component({
@@ -15,89 +15,54 @@ import { Project } from 'src/app/models/project';
   styleUrls: ['./project-list.component.sass'],
 })
 export class ProjectListComponent implements OnInit {
-  /**
-   * Dummy project list. Only necessary fields are populated.
-   */
-  public projects: Project[] = [
-    {
-      id: '001',
-      key: 'P001',
-      name: 'Project 001',
-      desc: 'Project 001 description',
-      iconId: 'ico_p001',
-      category: {
-        id: 'cat001',
-        name: 'Category 001',
-        desc: 'Project category 001',
-      },
-      components: [],
-      versions: [],
-    },
-    {
-      id: '002',
-      key: 'P002',
-      name: 'Project 002',
-      desc: 'Project 002 description',
-      iconId: 'ico_p002',
-      category: {
-        id: 'cat001',
-        name: 'Category 001',
-        desc: 'Project category 001',
-      },
-      components: [],
-      versions: [],
-    },
-    {
-      id: '003',
-      key: 'P003',
-      name: 'Project 003',
-      desc: 'Project 003 description',
-      iconId: 'ico_p003',
-      category: {
-        id: 'cat002',
-        name: 'Category 002',
-        desc: 'Project category 002',
-      },
-      components: [],
-      versions: [],
-    },
-    {
-      id: '004',
-      key: 'P004',
-      name: 'Project 004',
-      desc: 'Project 004 description',
-      iconId: 'ico_p004',
-      category: {
-        id: 'cat002',
-        name: 'Category 002',
-        desc: 'Project category 002',
-      },
-      components: [],
-      versions: [],
-    },
-    {
-      id: '011',
-      key: 'P011',
-      name: 'Project 011',
-      desc: 'Project 011 description',
-      iconId: 'ico_p011',
-      category: {
-        id: 'cat003',
-        name: 'Category 003',
-        desc: 'Project category 003',
-      },
-      components: [],
-      versions: [],
-    },
-  ];
+  @Input()
+  projects: Project[];
 
   /**
-   * Filter text for project name(s)
+   * Dictionary mapping project-id to checkbox state.
+   */
+  options: Map<string, boolean> = new Map();
+
+  /**
+   * Filter text for project name(s).
    */
   filterText: string;
+
+  /**
+   * Number of projects selected.
+   */
+  selected = 0;
 
   constructor() { }
 
   ngOnInit() {
+    this.projects.map((project) => {
+      this.options.set(project.id, false);
+    });
+  }
+
+  toggleProject(id: string) {
+    const flag = !this.options.get(id);
+
+    this.options.set(id, flag);
+
+    if (flag) {
+      this.selected++;
+    } else {
+      this.selected--;
+    }
+  }
+
+  setFavorites() {
+    const selection: string[] = [];
+
+    this.options.forEach((v, k) => {
+      if (v) {
+        selection.push(k);
+      }
+    });
+
+    console.log('Dispatch action');
+    console.log(selection);
   }
 }
