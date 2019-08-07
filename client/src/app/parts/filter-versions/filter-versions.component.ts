f73caeee8bf314406a6d7f311d00d85bb2daf5d3
Sanjeev Premi <spremi@ymail.com>
2019-08-07 23:46:54 +0530

    feat: c: parts: filter-versions: Initial implementation
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- client/src/app/parts/filter-versions/filter-versions.component.ts
+++ client/src/app/parts/filter-versions/filter-versions.component.ts
@@ -6,7 +6,9 @@
 // Available under terms of the BSD-3-Clause license.
 //
 
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { MatListOption } from '@angular/material/list';
+import { ProjectVersion } from 'src/app/models/project';
 
 @Component({
   selector: 'sp-filter-versions',
@@ -14,10 +16,51 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./filter-versions.component.sass'],
 })
 export class FilterVersionsComponent implements OnInit {
+  @Input()
+  title: string;
+
+  /**
+   * Dummy list of project versions.
+   */
+  list: ProjectVersion[] = [
+    {
+      id: 'vers01',
+      name: 'Vers 1',
+      desc: 'Version 1',
+      released: true,
+    },
+    {
+      id: 'vers01',
+      name: 'Vers 1',
+      desc: 'Version 1',
+      released: true,
+    },
+    {
+      id: 'vers02',
+      name: 'Vers 2',
+      desc: 'Version 2',
+      released: true,
+    },
+    {
+      id: 'vers03',
+      name: 'Vers 3',
+      desc: 'Version 3',
+      released: true,
+    },
+    {
+      id: 'vers04',
+      name: 'Vers 4',
+      desc: 'Version 4',
+      released: false,
+    },
+  ];
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  applyFilter(selected: MatListOption[]) {
+    console.log('TODO: Dispatch action');
+  }
 }
