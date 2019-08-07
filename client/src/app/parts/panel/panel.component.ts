e6d7c34812e35d93721de9cd13ea74ee6ca8e59b
Sanjeev Premi <spremi@ymail.com>
2019-08-07 22:11:49 +0530

    chore: c: parts: Add 'panel'
    Command:
      ng g c parts/panel --style=sass --lintFix=true
    
    Also added file headers.
    
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

---
+++ client/src/app/parts/panel/panel.component.ts
@@ -0,0 +1,23 @@
+//
+// [insight]
+//
+// Sanjeev Premi <spremi@ymail.com>
+//
+// Available under terms of the BSD-3-Clause license.
+//
+
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'sp-panel',
+  templateUrl: './panel.component.html',
+  styleUrls: ['./panel.component.sass'],
+})
+export class PanelComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
