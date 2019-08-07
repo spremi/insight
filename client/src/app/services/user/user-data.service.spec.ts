4fc032984c3b4eb9df296d68f136037f2fb268fc
Sanjeev Premi <spremi@ymail.com>
2019-08-08 01:32:58 +0530

    chore: c: svc: Add 'user-data'
    Command:
      ng g s services/user/user-data --lintFix=true
    
    Also added file headers.
    
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

---
+++ client/src/app/services/user/user-data.service.spec.ts
@@ -0,0 +1,20 @@
+//
+// [insight]
+//
+// Sanjeev Premi <spremi@ymail.com>
+//
+// Available under terms of the BSD-3-Clause license.
+//
+
+import { TestBed } from '@angular/core/testing';
+
+import { UserDataService } from './user-data.service';
+
+describe('UserDataService', () => {
+  beforeEach(() => TestBed.configureTestingModule({}));
+
+  it('should be created', () => {
+    const service: UserDataService = TestBed.get(UserDataService);
+    expect(service).toBeTruthy();
+  });
+});
