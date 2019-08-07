02d509b2154952fa095d5c082613e98ea0b8c192
Sanjeev Premi <spremi@ymail.com>
2019-08-08 01:43:16 +0530

    feat: c: svc: user-data: Initial implementation
    For now, proides dummy data.
    
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- client/src/app/services/user/user-data.service.ts
+++ client/src/app/services/user/user-data.service.ts
@@ -7,11 +7,56 @@
 //
 
 import { Injectable } from '@angular/core';
+import { of, Observable } from 'rxjs';
+import { User } from 'src/app/models/user';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UserDataService {
+  /**
+   * Dummy list of users.
+   */
+  readonly users: User[] = [
+    {
+      id: 'user01',
+      name: 'User 1',
+      avatarId: 'av001',
+      active: true,
+    },
+    {
+      id: 'user02',
+      name: 'User 2',
+      avatarId: 'av002',
+      active: true,
+    },
+    {
+      id: 'user03',
+      name: 'User 3',
+      avatarId: 'av003',
+      active: true,
+    },
+    {
+      id: 'user04',
+      name: 'User 4',
+      avatarId: 'av004',
+      active: true,
+    },
+    {
+      id: 'user05',
+      name: 'User 5',
+      avatarId: 'av005',
+      active: true,
+    },
+  ];
 
   constructor() { }
+
+  getUser(id: string): Observable<User> {
+    return of(this.users[0]);
+  }
+
+  getUsers(projId: string): Observable<User[]> {
+    return of(this.users);
+  }
 }
