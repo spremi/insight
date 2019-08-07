90d977e2737679ad88cc1b244c00f160c26461d5
Sanjeev Premi <spremi@ymail.com>
2019-08-08 01:12:04 +0530

    feat: c: parts: login: Initial implementation
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- client/src/app/parts/login/login.component.spec.ts
+++ client/src/app/parts/login/login.component.spec.ts
@@ -8,6 +8,9 @@
 
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from 'src/app/modules/material/material.module';
 import { LoginComponent } from './login.component';
 
 describe('LoginComponent', () => {
@@ -16,6 +19,12 @@ describe('LoginComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [
+        NoopAnimationsModule,
+        MaterialModule,
+        FormsModule,
+        ReactiveFormsModule,
+      ],
       declarations: [LoginComponent],
     })
       .compileComponents();
--- client/src/app/parts/login/login.component.ts
+++ client/src/app/parts/login/login.component.ts
@@ -7,6 +7,7 @@
 //
 
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'sp-login',
@@ -14,10 +15,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.sass'],
 })
 export class LoginComponent implements OnInit {
+  loginForm = this.fb.group({
+    user: ['', Validators.required],
+    pass: ['', Validators.required],
+  });
 
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
   }
 
+  onCancel() {
+    this.loginForm.setValue({
+      user: '',
+      pass: '',
+    });
+
+    this.loginForm.markAsUntouched();
+  }
+
+  onlogin() {
+    const payload = {
+      id: this.loginForm.get('user').value,
+      pass: this.loginForm.get('pass').value,
+    };
+
+    console.log('TODO: Dispatch action');
+  }
 }
