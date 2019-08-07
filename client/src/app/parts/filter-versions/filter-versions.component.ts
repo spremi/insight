ddb4ce08cfa08e2e716ba0b8a90bd294e8e254a3
Sanjeev Premi <spremi@ymail.com>
2019-08-07 23:46:54 +0530

    feat: c: parts: filter-versions: Initial implementation
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- client/src/app/parts/filter-versions/filter-versions.component.ts
+++ client/src/app/parts/filter-versions/filter-versions.component.ts
@@ -6,7 +6,10 @@
 // Available under terms of the BSD-3-Clause license.
 //
 
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { MatListOption } from '@angular/material/list';
+import { ProjectVersion } from 'src/app/models/project';
+import { ProjectDataService } from 'src/app/services/project/project-data.service';
 
 @Component({
   selector: 'sp-filter-versions',
@@ -14,10 +17,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./filter-versions.component.sass'],
 })
 export class FilterVersionsComponent implements OnInit {
+  @Input()
+  title: string;
 
-  constructor() { }
+  /**
+   * List of versions.
+   */
+  list: ProjectVersion[];
+
+  constructor(private projDataSvc: ProjectDataService) { }
 
   ngOnInit() {
+    this.projDataSvc.getVersions().subscribe((result) => {
+      this.list = result;
+    });
   }
 
+  applyFilter(selected: MatListOption[]) {
+    console.log('TODO: Dispatch action');
+  }
 }
