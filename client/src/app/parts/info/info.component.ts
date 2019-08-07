ac474fc5c093ff94bf771cf0013166e0859dd714
Sanjeev Premi <spremi@ymail.com>
2019-08-07 22:09:18 +0530

    chore: c: parts: Add 'info'
    Command:
      ng g c parts/info --style=sass --lintFix=true
    
    Also added file headers.
    
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

---
+++ client/src/app/parts/info/info.component.ts
@@ -0,0 +1,23 @@
+//
+// [insight]
+//
+// Sanjeev Premi <spremi@ymail.com>
+//
+// Available under terms of the BSD-3-Clause license.
+//
+
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'sp-info',
+  templateUrl: './info.component.html',
+  styleUrls: ['./info.component.sass'],
+})
+export class InfoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
