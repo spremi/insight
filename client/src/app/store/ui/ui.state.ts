1620e86bfabbf145600999cdf95b0aa752481b3f
Sanjeev Premi <spremi@ymail.com>
2019-08-13 22:29:47 +0530

    feat: c: ui(select): Join project list selector with
    favorites
    
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- client/src/app/store/ui/ui.state.ts
+++ client/src/app/store/ui/ui.state.ts
@@ -7,6 +7,8 @@
 //
 
 import { Action, Selector, State, StateContext } from '@ngxs/store';
+import { Project } from 'src/app/models/project';
+import { ProjectState } from '../project/project.state';
 import {
   UiDummy, UiFetchFavorites, UiRemoveFavorite, UiSetAffectedVersions,
   UiSetFavorites, UiSetFixVersions, UiSetIssue, UiSetProject, UiSetWaiting
@@ -55,9 +57,13 @@ export class UiState {
     return state.waiting;
   }
 
-  @Selector()
-  public static getFavProjects(state: UiStateModel): string[] {
-    return state.favProjects;
+  @Selector([ProjectState.getList])
+  public static getFavProjects(state: UiStateModel, projects: Project[]): Project[] {
+    const favProjects = state.favProjects;
+
+    return projects.filter((proj) => {
+      return favProjects.some((fav) => proj.id === fav);
+    });
   }
 
   @Selector()
