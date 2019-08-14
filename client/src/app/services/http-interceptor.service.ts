6392f135d4f36de55b1ff6ff240d11c966154585
Sanjeev Premi <spremi@ymail.com>
2019-08-15 01:37:58 +0530

    feat: c: svc: http-interceptor: Initial implementation
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- client/src/app/services/http-interceptor.service.ts
+++ client/src/app/services/http-interceptor.service.ts
@@ -6,12 +6,64 @@
 // Available under terms of the BSD-3-Clause license.
 //
 
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Store } from '@ngxs/store';
+import { Observable, Subscription } from 'rxjs';
+import { AuthSession } from '../models/remote';
+import { UserState } from '../store/user/user.state';
 
 @Injectable({
   providedIn: 'root',
 })
-export class HttpInterceptorService {
+export class HttpInterceptorService implements HttpInterceptor {
+  private token: string;
+  private session: AuthSession;
 
-  constructor() { }
+  private subs: Subscription[] = [];
+
+  constructor(private store: Store) {
+    this.subs.push(
+      this.store.select(UserState.getToken).subscribe((arg) => {
+        this.token = arg;
+      }));
+
+    this.subs.push(
+      this.store.select(UserState.getSession).subscribe((arg) => {
+        this.session = arg;
+      }));
+  }
+
+  ngOnDestory() {
+    this.subs.forEach((sub) => {
+      if (sub) {
+        sub.unsubscribe();
+      }
+    });
+  }
+
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    if (!req.headers.has('Accept')) {
+      req = req.clone({
+        headers: req.headers.set('Accept', 'application/json'),
+      });
+    }
+
+    if (this.token) {
+      req = req.clone({
+        headers: req.headers.set('Authorization', 'Bearer ' + this.token),
+      });
+    }
+
+    if (this.session) {
+      req = req.clone({
+        headers: req.headers.set('insight-sess-n', this.session.name),
+      });
+      req = req.clone({
+        headers: req.headers.set('insight-sess-v', this.session.value),
+      });
+    }
+
+    return next.handle(req);
+  }
 }
