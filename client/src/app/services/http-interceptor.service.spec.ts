1c566e4033d9206c0adccdf12b4b2f8975597b72
Sanjeev Premi <spremi@ymail.com>
2019-08-15 01:24:27 +0530

    chore: c: svc: Add 'http-interceptor'
    Command:
      ng g s services/http-interceptor --lintFix=true
    
    (Intentionally created at this level...)
    
    Also added file headers.
    
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

---
+++ client/src/app/services/http-interceptor.service.spec.ts
@@ -0,0 +1,20 @@
+//
+// [insight]
+//
+// Sanjeev Premi <spremi@ymail.com>
+//
+// Available under terms of the BSD-3-Clause license.
+//
+
+import { TestBed } from '@angular/core/testing';
+
+import { HttpInterceptorService } from './http-interceptor.service';
+
+describe('HttpInterceptorService', () => {
+  beforeEach(() => TestBed.configureTestingModule({}));
+
+  it('should be created', () => {
+    const service: HttpInterceptorService = TestBed.get(HttpInterceptorService);
+    expect(service).toBeTruthy();
+  });
+});
