b2132c9fd55f91f7c1533c78c06a35af3de51f71
Sanjeev Premi <spremi@ymail.com>
2019-08-15 01:39:33 +0530

    feat: c: app: Enable http interceptor
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgxsReduxDevtoolsPluginModule } from '@ngxs/devtools-plugin';
@@ -43,6 +43,7 @@ import { HomeComponent } from './routes/home/home.component';
 import { IssueComponent } from './routes/issue/issue.component';
 import { ProjectComponent } from './routes/project/project.component';
 import { StrayComponent } from './routes/stray/stray.component';
+import { HttpInterceptorService } from './services/http-interceptor.service';
 import { AppState } from './store';
 
 @NgModule({
@@ -98,7 +99,13 @@ import { AppState } from './store';
       disabled: environment.production,
     }),
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: HttpInterceptorService,
+      multi: true,
+    },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule { }
