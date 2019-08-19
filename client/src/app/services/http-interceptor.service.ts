ad41a28a054624d5ad5bba265b5277a368dad7f8
Sanjeev Premi <spremi@ymail.com>
2019-08-19 23:18:27 +0530

    feat: c: svc: http-interceptor: Add headers based on filters
    There is no need to provide relevant filters with each request.
    
    Appropriate filters shall be used in the server-side processing.
    
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- client/src/app/services/http-interceptor.service.ts
+++ client/src/app/services/http-interceptor.service.ts
@@ -11,6 +11,7 @@ import { Injectable } from '@angular/core';
 import { Store } from '@ngxs/store';
 import { Observable, Subscription } from 'rxjs';
 import { AuthSession } from '../models/remote';
+import { FilterState } from '../store';
 import { UserState } from '../store/user/user.state';
 
 @Injectable({
@@ -19,6 +20,15 @@ import { UserState } from '../store/user/user.state';
 export class HttpInterceptorService implements HttpInterceptor {
   private token: string;
   private session: AuthSession;
+
+  private components: string[];
+  private users: string[];
+  private types: string[];
+  private statuses: string[];
+  private priorities: string[];
+  private resolutions: string[];
+  private fixVersions: string[];
+  private affectedVersions: string[];
 
   private subs: Subscription[] = [];
 
@@ -31,6 +41,46 @@ export class HttpInterceptorService implements HttpInterceptor {
     this.subs.push(
       this.store.select(UserState.getSession).subscribe((arg) => {
         this.session = arg;
+      }));
+
+    this.subs.push(
+      this.store.select(FilterState.getComponents).subscribe((arg) => {
+        this.components = arg;
+      }));
+
+    this.subs.push(
+      this.store.select(FilterState.getUsers).subscribe((arg) => {
+        this.users = arg;
+      }));
+
+    this.subs.push(
+      this.store.select(FilterState.getTypes).subscribe((arg) => {
+        this.types = arg;
+      }));
+
+    this.subs.push(
+      this.store.select(FilterState.getStatuses).subscribe((arg) => {
+        this.statuses = arg;
+      }));
+
+    this.subs.push(
+      this.store.select(FilterState.getPriorities).subscribe((arg) => {
+        this.priorities = arg;
+      }));
+
+    this.subs.push(
+      this.store.select(FilterState.getResolutions).subscribe((arg) => {
+        this.resolutions = arg;
+      }));
+
+    this.subs.push(
+      this.store.select(FilterState.getFixVersions).subscribe((arg) => {
+        this.fixVersions = arg;
+      }));
+
+    this.subs.push(
+      this.store.select(FilterState.getAffectedVersions).subscribe((arg) => {
+        this.affectedVersions = arg;
       }));
   }
 
@@ -62,6 +112,54 @@ export class HttpInterceptorService implements HttpInterceptor {
       req = req.clone({
         headers: req.headers.set('insight-sess-v', this.session.value),
       });
+
+      if (this.components && this.components.length > 0) {
+        req = req.clone({
+          headers: req.headers.set('insight-comps', this.components.join(',')),
+        });
+      }
+
+      if (this.users && this.users.length > 0) {
+        req = req.clone({
+          headers: req.headers.set('insight-users', this.users.join(',')),
+        });
+      }
+
+      if (this.types && this.types.length > 0) {
+        req = req.clone({
+          headers: req.headers.set('insight-types', this.types.join(',')),
+        });
+      }
+
+      if (this.statuses && this.statuses.length > 0) {
+        req = req.clone({
+          headers: req.headers.set('insight-statuses', this.statuses.join(',')),
+        });
+      }
+
+      if (this.priorities && this.priorities.length > 0) {
+        req = req.clone({
+          headers: req.headers.set('insight-priorities', this.priorities.join(',')),
+        });
+      }
+
+      if (this.resolutions && this.resolutions.length > 0) {
+        req = req.clone({
+          headers: req.headers.set('insight-resolutions', this.resolutions.join(',')),
+        });
+      }
+
+      if (this.fixVersions && this.fixVersions.length > 0) {
+        req = req.clone({
+          headers: req.headers.set('insight-fix-versions', this.fixVersions.join(',')),
+        });
+      }
+
+      if (this.affectedVersions && this.affectedVersions.length > 0) {
+        req = req.clone({
+          headers: req.headers.set('insight-aff-versions', this.affectedVersions.join(',')),
+        });
+      }
     }
 
     return next.handle(req);
