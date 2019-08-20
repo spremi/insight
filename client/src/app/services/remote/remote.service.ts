e0cc9d07ee006b50bd5e61d679da5261391418e7
Sanjeev Premi <spremi@ymail.com>
2019-08-21 01:53:01 +0530

    feat: c: svc: remote: Initial implementation
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- client/src/app/services/remote/remote.service.ts
+++ client/src/app/services/remote/remote.service.ts
@@ -12,6 +12,71 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class RemoteService {
+  private readonly LOGIN = '/login';
+  private readonly LOGOUT = '/logout';
+
+  private readonly USERS = '/users';
+  private readonly PROJECTS = '/projects';
+  private readonly ISSUES = '/issues';
 
   constructor() { }
+
+  /**
+   * Returns login url
+   */
+  loginUrl(): string {
+    return this.LOGIN;
+  }
+
+  /**
+   * Returns logout url
+   */
+  logoutUrl(): string {
+    return this.LOGOUT;
+  }
+
+  /**
+   * Returns url for user resources.
+   */
+  userUrl(id?: string, res?: string): string {
+    if (id && res) {
+      return this.USERS + '/' + id + '/' + res;
+    }
+
+    if (id) {
+      return this.USERS + '/' + id;
+    }
+
+    return this.USERS;
+  }
+
+  /**
+   * Returns url for project resources.
+   */
+  projectUrl(id?: string, res?: string): string {
+    if (id && res) {
+      return this.PROJECTS + '/' + id + '/' + res;
+    }
+
+    if (id) {
+      return this.PROJECTS + '/' + id;
+    }
+
+    return this.PROJECTS;
+  }
+
+  /**
+   * Returns url for issue resources.
+   */
+  issueUrl(id?: string, res?: string): string {
+    if (id && res) {
+      return this.ISSUES + '/' + id + '/' + res;
+    }
+
+    if (id) {
+      return this.ISSUES + '/' + id;
+    }
+
+    return this.ISSUES;
+  }
 }
