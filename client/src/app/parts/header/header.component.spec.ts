a09993b486632d9ab4b48526b850bafe5f950d95
Sanjeev Premi <spremi@ymail.com>
2019-08-21 01:24:25 +0530

    feat: c: parts: header: Show current user & project
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- client/src/app/parts/header/header.component.spec.ts
+++ client/src/app/parts/header/header.component.spec.ts
@@ -10,6 +10,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterTestingModule } from '@angular/router/testing';
 import { NgxsModule } from '@ngxs/store';
 import { MaterialModule } from 'src/app/modules/material/material.module';
 import { AppState } from 'src/app/store';
@@ -24,6 +25,7 @@ describe('HeaderComponent', () => {
       imports: [
         NoopAnimationsModule,
         MaterialModule,
+        RouterTestingModule,
         HttpClientTestingModule,
         NgxsModule.forRoot(AppState),
       ],
--- client/src/app/parts/header/header.component.ts
+++ client/src/app/parts/header/header.component.ts
@@ -7,9 +7,15 @@
 //
 
 import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 import { Select, Store } from '@ngxs/store';
 import { Observable } from 'rxjs';
-import { UserLogout, UserState } from 'src/app/store';
+import { filter } from 'rxjs/operators';
+import { Project } from 'src/app/models/project';
+import { User } from 'src/app/models/user';
+import { ProjectState } from 'src/app/store/project/project.state';
+import { UserLogout } from 'src/app/store/user/user.actions';
+import { UserState } from 'src/app/store/user/user.state';
 
 @Component({
   selector: 'sp-header',
@@ -22,9 +28,22 @@ export class HeaderComponent implements OnInit {
    */
   @Select(UserState.isAuthenticated) isAuth$: Observable<boolean>;
 
-  constructor(private store: Store) { }
+  @Select(UserState.getUser) user$: Observable<User>;
+  @Select(ProjectState.getSelected) proj$: Observable<Project>;
+
+  /**
+   * Current URL.
+   */
+  currentUrl = '';
+
+  constructor(private store: Store, private router: Router) { }
 
   ngOnInit() {
+    this.router.events.pipe(
+      filter((event) => event instanceof NavigationEnd)
+    ).subscribe((event: NavigationEnd) => {
+      this.currentUrl = event.url;
+    });
   }
 
   logout() {
