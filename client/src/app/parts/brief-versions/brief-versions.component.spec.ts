797facf9b705dd28ca53ce5895525e4cd240c659
Sanjeev Premi <spremi@ymail.com>
2019-08-21 02:08:55 +0530

    feat: c: parts: brief-versions: Initial implementation
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- client/src/app/parts/brief-versions/brief-versions.component.spec.ts
+++ client/src/app/parts/brief-versions/brief-versions.component.spec.ts
@@ -8,6 +8,8 @@
 
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from 'src/app/modules/material/material.module';
 import { BriefVersionsComponent } from './brief-versions.component';
 
 describe('BriefVersionsComponent', () => {
@@ -16,6 +18,10 @@ describe('BriefVersionsComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [
+        NoopAnimationsModule,
+        MaterialModule,
+      ],
       declarations: [BriefVersionsComponent],
     })
       .compileComponents();
--- client/src/app/parts/brief-versions/brief-versions.component.ts
+++ client/src/app/parts/brief-versions/brief-versions.component.ts
@@ -6,18 +6,76 @@
 // Available under terms of the BSD-3-Clause license.
 //
 
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { Select } from '@ngxs/store';
+import { Observable, Subscription } from 'rxjs';
+import { filter } from 'rxjs/operators';
+import { IssuesByVersion } from 'src/app/models/project';
+import { ProjectState } from 'src/app/store/project/project.state';
+
+interface VersionData {
+  label: string;
+  count: number;
+  affecting: number;
+  fixed: number;
+  unresolved: number;
+}
 
 @Component({
   selector: 'sp-brief-versions',
   templateUrl: './brief-versions.component.html',
   styleUrls: ['./brief-versions.component.sass'],
 })
-export class BriefVersionsComponent implements OnInit {
+export class BriefVersionsComponent implements OnInit, OnDestroy {
+  @Select(ProjectState.getIssuesByVersion) data$: Observable<IssuesByVersion[]>;
+
+  private sub: Subscription;
+
+  /**
+   * Columns to display.
+   */
+  public readonly cols: string[] = [
+    'label',
+    'count',
+    'affecting',
+    'fixed',
+    'unresolved',
+  ];
+
+  /**
+   * Data to display.
+   */
+  public data: VersionData[] = [];
+
+  /**
+   * Data source for the table.
+   */
+  public dataSource: MatTableDataSource<VersionData>;
 
   constructor() { }
 
   ngOnInit() {
+    this.sub = this.data$.pipe(
+      filter((arg) => !!arg)
+    ).subscribe((list: IssuesByVersion[]) => {
+      list.forEach((item) => {
+        this.data.push({
+          label: item.version.name,
+          count: item.data.count,
+          affecting: item.data.affecting,
+          fixed: item.data.fixed,
+          unresolved: item.data.unresolved,
+        });
+      });
+    });
+
+    this.dataSource = new MatTableDataSource(this.data);
   }
 
+  ngOnDestroy() {
+    if (this.sub) {
+      this.sub.unsubscribe();
+    }
+  }
 }
