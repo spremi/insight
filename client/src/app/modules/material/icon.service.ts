e362dc871f49df58ce8b3d45618c83cc88c45803
Sanjeev Premi <spremi@ymail.com>
2019-09-14 19:52:46 +0530

    feat: c: module: material: icon-svc: Implement register()
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- client/src/app/modules/material/icon.service.ts
+++ client/src/app/modules/material/icon.service.ts
@@ -7,11 +7,39 @@
 //
 
 import { Injectable } from '@angular/core';
+import { MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
+import { AppIcons, MdiIcons } from './icon-list';
+import { IconInfo, IconSet } from './icon-set';
 
 @Injectable({
   providedIn: 'root',
 })
 export class IconService {
 
-  constructor() { }
+  constructor(private registry: MatIconRegistry, private sanitizer: DomSanitizer) { }
+
+  /**
+   * Register all icons.
+   */
+  register() {
+    this.registerSet(AppIcons);
+    this.registerSet(MdiIcons);
+  }
+
+  /**
+   * Register icons in specified set.
+   */
+  private registerSet(set: IconSet) {
+    if (set.list.length === 0) {
+      return;
+    }
+
+    set.list.forEach((arg: IconInfo) => {
+      const iconPath = (set.path + '/' + arg.file).replace('//', '/');
+      this.registry.addSvgIcon(
+        arg.icon,
+        this.sanitizer.bypassSecurityTrustResourceUrl(iconPath));
+    });
+  }
 }
