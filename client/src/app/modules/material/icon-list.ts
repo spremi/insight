e84b2bdda1426b67f18a27d8eb0d65cab5c7520e
Sanjeev Premi <spremi@ymail.com>
2019-09-14 19:32:04 +0530

    feat: c: module: material: List initial set of icons
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

---
+++ client/src/app/modules/material/icon-list.ts
@@ -0,0 +1,70 @@
+//
+// [insight]
+//
+// Sanjeev Premi <spremi@ymail.com>
+//
+// Available under terms of the BSD-3-Clause license.
+//
+
+import { IconSet } from './icon-set';
+
+/**
+ * List of common icons.
+ */
+export const AppIcons: IconSet = {
+  path: '/assets/icons/',
+  list: [
+    {
+      icon: 'bookmark',
+      file: 'ic_bookmark_24px.svg',
+    },
+    {
+      icon: 'check',
+      file: 'ic_check_24px.svg',
+    },
+    {
+      icon: 'clear',
+      file: 'ic_clear_24px.svg',
+    },
+    {
+      icon: 'person',
+      file: 'ic_person_24px.svg',
+    },
+    {
+      icon: 'refresh',
+      file: 'ic_refresh_24px.svg',
+    },
+    {
+      icon: 'save',
+      file: 'ic_save_24px.svg',
+    },
+    {
+      icon: 'search',
+      file: 'ic_search_24px.svg',
+    },
+    {
+      icon: 'security',
+      file: 'ic_security_24px.svg',
+    }, {
+      icon: 'swap_horiz',
+      file: 'ic_swap_horiz_24px.svg',
+    },
+  ],
+};
+
+/**
+ * List of icons from MDI.
+ */
+export const MdiIcons: IconSet = {
+  path: '/assets/icons/mdi/',
+  list: [
+    {
+      icon: 'filter',
+      file: 'filter.svg',
+    },
+    {
+      icon: 'license',
+      file: 'license.svg',
+    },
+  ],
+};
