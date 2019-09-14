186f55209b615f03d5a0146748e7f620755c05cd
Sanjeev Premi <spremi@ymail.com>
2019-09-14 19:17:09 +0530

    feat: c: modules: material: Define interface 'IconSet'
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- client/src/app/modules/material/icon-set.ts
+++ client/src/app/modules/material/icon-set.ts
@@ -6,5 +6,24 @@
 // Available under terms of the BSD-3-Clause license.
 //
 
+/**
+ * Describes an icon.
+ */
+export interface IconInfo {
+  /** Name of the icon - used in code. */
+  readonly icon: string;
+
+  /** Corresponding file name. */
+  readonly file: string;
+}
+
+/**
+ * Describes a set of icons at specific path.
+ */
 export interface IconSet {
+  /** Path to base of icon set. */
+  readonly path: string;
+
+  /** Array of icons. */
+  readonly list: IconInfo[];
 }
